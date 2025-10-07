37a0a6ef49acaff43819be713964a68d427e92f6
Tunar Ismayilzada <ismayilzadetunar2004@gmail.com>
2025-10-07 14:34:11 +0400

    error fixed

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+// const { Telegraf } = require("telegraf");
+// const youtubedl = require("youtube-dl-exec");
+// const fs = require("fs");
+// const path = require("path");
+
+// // const bot = new Telegraf("8353240854:AAGrhzzxJEO8lfebDETqEszz-O5ysC3C0k0");
+
+// // const ffmpegPath = "C:\\ffmpeg-8.0-essentials_build\\bin\\ffmpeg.exe";
+// const bot = new Telegraf(process.env.BOT_TOKEN);
+// const ffmpegPath = process.env.FFMPEG_PATH;
+
+// bot.start((ctx) =>
+//   ctx.reply("Salam! 🎶 Mənə YouTube link göndər, sənin üçün mahnını mp3 şəklində yükləyim.")
+// );
+
+// bot.on("text", async (ctx) => {
+//   try {
+//     const url = ctx.message.text;
+
+//     if (!url.startsWith("http")) {
+//       return ctx.reply("❌ Zəhmət olmasa keçərli YouTube link göndər.");
+//     }
+
+//     ctx.reply("🎧 Mahnı yüklənir, bir az gözlə...");
+
+//     const outputTemplate = path.resolve(__dirname, "%(title)s.%(ext)s");
+
+//     await youtubedl(url, {
+//       extractAudio: true,
+//       audioFormat: "mp3",
+//       audioQuality: 0,
+//       output: outputTemplate,
+//       ffmpegLocation: ffmpegPath,
+//     });
+
+
+//     const info = await youtubedl(url, { dumpSingleJson: true });
+//     const title = info.title.replace(/[\\/:*?"<>|]/g, ""); 
+//     const filePath = path.resolve(__dirname, `${title}.mp3`);
+
+
+//     await ctx.replyWithAudio({ source: filePath });
+//     fs.unlinkSync(filePath);
+//   } catch (error) {
+//     console.error("Xəta:", error);
+//     ctx.reply("❌ Xəta baş verdi, bir az sonra yenidən cəhd et.");
+//   }
+// });
+
+// bot.launch();
+// console.log("✅ Bot işə düşdü!");
+
+
 const { Telegraf } = require("telegraf");
 const youtubedl = require("youtube-dl-exec");
 const fs = require("fs");
 const path = require("path");
 
-// const bot = new Telegraf("8353240854:AAGrhzzxJEO8lfebDETqEszz-O5ysC3C0k0");
-
-// const ffmpegPath = "C:\\ffmpeg-8.0-essentials_build\\bin\\ffmpeg.exe";
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const ffmpegPath = process.env.FFMPEG_PATH;
+const ytDlpPath = process.env.YTDLP_PATH;
 
 bot.start((ctx) =>
   ctx.reply("Salam! 🎶 Mənə YouTube link göndər, sənin üçün mahnını mp3 şəklində yükləyim.")
@@ -31,13 +82,12 @@ bot.on("text", async (ctx) => {
       audioQuality: 0,
       output: outputTemplate,
       ffmpegLocation: ffmpegPath,
+      youtubeDL: ytDlpPath,
     });
 
-
-    const info = await youtubedl(url, { dumpSingleJson: true });
-    const title = info.title.replace(/[\\/:*?"<>|]/g, ""); 
+    const info = await youtubedl(url, { dumpSingleJson: true, youtubeDL: ytDlpPath });
+    const title = info.title.replace(/[\\/:*?"<>|]/g, "");
     const filePath = path.resolve(__dirname, `${title}.mp3`);
-
 
     await ctx.replyWithAudio({ source: filePath });
     fs.unlinkSync(filePath);
